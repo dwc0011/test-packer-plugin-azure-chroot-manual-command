1d2bb9f15ee3ba745687874939a7804a91999f7a
dwc0011 <dwc0011@user.noreply.github.com>
2025-09-17 11:19:54 -0400

    Enusre path is not expanded

--- main.tf
+++ main.tf
@@ -153,13 +153,17 @@ resource "azurerm_linux_virtual_machine" "this" {
     dnf update -y
     dnf install -y dnf-utils wget unzip curl gnupg jq git go-toolset lvm2
     dnf install cloud-utils-growpart gdisk
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    dnf install -y https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm
+    dnf install -y azure-cli
+
 
     mkdir -p /packerbuild/test-resources
   
 
     echo "#!/bin/bash" > /packerbuild/build_packer.sh
     echo "cd /packerbuild/${var.packer_plugin_name}" >> /packerbuild/build_packer.sh
-    echo "export PATH=/usr/local/bin/:$PATH" >> /packerbuild/build_packer.sh
+    echo 'export PATH=/usr/local/bin/:$PATH' >> /packerbuild/build_packer.sh
     echo "make dev" >> /packerbuild/build_packer.sh
     chmod +x /packerbuild/build_packer.sh
 
@@ -167,7 +171,7 @@ resource "azurerm_linux_virtual_machine" "this" {
     echo "cd /packerbuild" >> /packerbuild/run_packer.sh    
     echo "export PACKER_LOG=1" >> /packerbuild/run_packer.sh
     echo "export PACKER_LOG_PATH=/packerbuild/packer.log" >> /packerbuild/run_packer.sh
-    echo "export PATH=/usr/local/bin/:$PATH" >> /packerbuild/run_packer.sh
+    echo 'export PATH=/usr/local/bin/:$PATH' >> /packerbuild/run_packer.sh
     echo "packer init azure-chroot.pkr.hcl" >> /packerbuild/run_packer.sh
     echo "packer build --var subscription_id=${data.azurerm_subscription.current.subscription_id} --var resource_group=${local.resource_group} --var location=${var.location} azure-chroot.pkr.hcl &" >> /packerbuild/run_packer.sh
     chmod +x /packerbuild/run_packer.sh
@@ -176,7 +180,7 @@ resource "azurerm_linux_virtual_machine" "this" {
     echo "cd /packerbuild" >> /packerbuild/validate_packer.sh
     echo "export PACKER_LOG=1" >> /packerbuild/validate_packer.sh
     echo "export PACKER_LOG_PATH=/packerbuild/packer.log" >> /packerbuild/validate_packer.sh
-    echo "export PATH=/usr/local/bin/:$PATH" >> /packerbuild/validate_packer.sh
+    echo 'export PATH=/usr/local/bin/:$PATH' >> /packerbuild/validate_packer.sh
     echo "packer init azure-chroot.pkr.hcl" >> /packerbuild/validate_packer.sh
     echo "packer validate --var subscription_id=${data.azurerm_subscription.current.subscription_id} --var resource_group=${local.resource_group} --var location=${var.location} azure-chroot.pkr.hcl" >> /packerbuild/validate_packer.sh
     chmod +x /packerbuild/validate_packer.sh
@@ -203,9 +207,6 @@ resource "azurerm_linux_virtual_machine" "this" {
     
     /packerbuild/test-resources/scripts/resize.sh
 
-    rpm --import https://packages.microsoft.com/keys/microsoft.asc
-    dnf install -y https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm
-    dnf install azure-cli
   EOF
   )
 
