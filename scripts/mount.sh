6bdffaa31a908d61e9817d0a4ac793988e23eed0
dwc0011 <dwc0011@user.noreply.github.com>
2025-09-24 10:16:55 -0400

    Change the AMIGEN9 Source to use the branch that has AzureUtils.sh

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -4,14 +4,13 @@ set -euxo pipefail
 export DNF_VAR_ocidomain="oracle.com"
 export DNF_VAR_ociregion=""
 
-
-
 export SPEL_AMIGENBRANCH="main"
 export SPEL_AMIGENPKGGRP="core"
 export SPEL_AMIGENREPOS="spel,packages-microsoft-com-prod,rhel-9-for-x86_64-appstream-rhui-rpms,rhel-9-for-x86_64-baseos-rhui-rpms,rhel-9-for-x86_64-supplementary-rhui-rpms,rhui-microsoft-azure-rhel9"
 export SPEL_USEDEFAULTREPOS="false"
 export SPEL_AMIGENREPOSRC="https://spel-packages.cloudarmor.io/spel-packages/repo/spel-release-latest-9.noarch.rpm"
-export SPEL_AMIGEN9SOURCE="https://github.com/plus3it/AMIgen9.git"
+export SPEL_AMIGEN9SOURCE="https://github.com/dwc0011/amigen9.git"
+export SPEL_AMIGENBRANCH="chroot"
 export SPEL_AMIGENSTORLAY="/:rootVol:6,swap:swapVol:2,/home:homeVol:1,/var:varVol:2,/var/tmp:varTmpVol:2,/var/log:logVol:2,/var/log/audit:auditVol:100%FREE"
 export SPEL_AMIGENVGNAME="VolGroup00"
 export SPEL_AMIUTILSSOURCE=""
@@ -34,7 +33,8 @@ export SPEL_AMIGENUEFIDEVLBL="UEFI_DISK"
 export SPEL_AMIGENUEFIDEVSZ="128"
 export SPEL_USEROOTDEVICE="false"
 
-
+dnf update -y --disablerepo='*' --enablerepo='*microsoft*'
+yum -y update
 /packerbuild/spel/spel/scripts/builder-prep-9.sh
 
 /packerbuild/spel/spel/scripts/amigen9-build.sh
